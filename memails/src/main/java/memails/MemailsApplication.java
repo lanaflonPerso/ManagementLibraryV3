af595c457cf0f52c84b808bb91d365bd7f6d9d22
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-11 10:30:52 +0200

    Mise à jour des poms

--- memails/src/main/java/memails/MemailsApplication.java
+++ memails/src/main/java/memails/MemailsApplication.java
@@ -2,8 +2,12 @@ package memails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class MemailsApplication {
 
 	public static void main(String[] args) {
--- musers/src/main/java/musers/MusersApplication.java
+++ musers/src/main/java/musers/MusersApplication.java
@@ -2,8 +2,12 @@ package musers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class MusersApplication {
 
 	public static void main(String[] args) {

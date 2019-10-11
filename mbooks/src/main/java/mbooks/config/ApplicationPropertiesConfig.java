14f96e9ae98600868acf6b5dc5eb2f39bb36f211
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-11 08:57:54 +0200

    Mise à jour des properties externes

--- mbooks/src/main/java/mbooks/config/ApplicationPropertiesConfig.java
+++ mbooks/src/main/java/mbooks/config/ApplicationPropertiesConfig.java
@@ -3,14 +3,12 @@ package mbooks.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("microservice.config")
 @Getter
 @Setter
-@RefreshScope
 public class ApplicationPropertiesConfig {
 
     private int renewalNumber;

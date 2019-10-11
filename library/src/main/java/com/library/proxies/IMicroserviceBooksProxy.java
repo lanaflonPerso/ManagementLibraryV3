5bc58e01ae4a74473b594dcb13b9f0f75fe62928
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-11 11:50:27 +0200

    Ajout de RIBBON et de zuul server

--- library/src/main/java/com/library/proxies/IMicroserviceBooksProxy.java
+++ library/src/main/java/com/library/proxies/IMicroserviceBooksProxy.java
@@ -1,12 +1,14 @@
 package com.library.proxies;
 
 import com.library.beans.mbooks.book.LanguageBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-books",url = "localhost:9001")
+@FeignClient(name = "microservice-books")
+@RibbonClient(name = "microservice-books")
 public interface IMicroserviceBooksProxy {
 
     @GetMapping("/Language/all")

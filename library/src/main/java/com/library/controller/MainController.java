9fca33567f184d9a149d7bf7886397c754ca7a31
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-14 09:35:11 +0200

    Ajout de sécurisation de zuul

--- library/src/main/java/com/library/controller/MainController.java
+++ library/src/main/java/com/library/controller/MainController.java
@@ -5,22 +5,33 @@ import com.library.proxies.IMicroserviceBooksProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-
 @Controller
-public class ClientController {
+public class MainController {
 
     @Autowired
     private IMicroserviceBooksProxy microserviceBooksProxy;
 
-    @RequestMapping("/")
-    public String accueil(Model model){
+    @GetMapping("/erreur")
+    public String erreur(Model model){
+        return "error/denied";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "user/login";
+    }
+
+    @GetMapping("/")
+    private String accueil(Model model){
         List<LanguageBean> languageList = microserviceBooksProxy.languageList();
         model.addAttribute("languageList",languageList);
-        return "Accueil";
+
+        return "index";
+
     }
 
 }

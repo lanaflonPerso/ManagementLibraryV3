79c140436b34073c966b58792870e1a7f20ca81b
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-12-11 13:29:09 +0100

    modification properties database mfile + création dump structure et data de mbooks, mfiles et musers

--- zipkin.cmd
+++ zipkin.cmd
@@ -1,1 +1,1 @@
-java -jar C:\Users\Alokin\workspace\projects\ManagementLibrary\zipkin-server-2.9.4-exec.jar
+java -jar C:\Users\Alokin\workspace\projects\ManagementLibraryV2\zipkin-server-2.9.4-exec.jar
